116f964101917f7b61eab8f43b58c51a7dc06366
MustafaKpn <mustafa.kpn@outlook.com>
2026-03-30 15:21:17 +0100

    Changed .git direcotry

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-# app/config/__init__.py
-from .settings import DATABASE_URL   # relative import
-from sqlalchemy import create_engine
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
--- config/settings.py
+++
@@ -1,7 +0,0 @@
-# app/config/settings.py
-import os
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://jobuser:jobpass@localhost:5432/jobsdb"
-)
